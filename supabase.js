749db6c938e2f04574e6b29199d1526fafa1b67a
dlola5101-debug <dlola5101@gmail.com>
2026-09-23 12:35:39 +0500

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,33 +1,14 @@
 window.VELIX_CONFIG = {
-  SUPABASE_URL: "https://kzkmlwprdjotbguwaclp.supabase.co",
+  SUPABASE_URL: https://kzkmlwprdjotbguwaclp.supabase.co
   SUPABASE_ANON_KEY: sb_publishable_r_KYRFaqP8P7pxT7tLuNCA_lMQia1t0
 };
-
 window.supabaseClient = null;
 
 window.initSupabase = function () {
   const { SUPABASE_URL, SUPABASE_ANON_KEY } = window.VELIX_CONFIG;
-
-  if (
-    !SUPABASE_URL ||
-    !SUPABASE_ANON_KEY ||
-    SUPABASE_URL.includes(https://kzkmlwprdjotbguwaclp.supabase.co) ||
-    SUPABASE_ANON_KEY.includes(sb_publishable_r_KYRFaqP8P7pxT7tLuNCA_lMQia1t0)
-  ) {
-    return false;
-  }
-
-  window.supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY,
-    {
-      auth: {
-        persistSession: true,
-        autoRefreshToken: true,
-        detectSessionInUrl: true
-      }
-    }
-  );
-
+  if (!SUPABASE_URL  SUPABASE_URL.includes("YOUR_PROJECT")  !SUPABASE_ANON_KEY || SUPABASE_ANON_KEY.includes("YOUR_")) return false;
+  window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+    auth: { persistSession: true, autoRefreshToken: true, detectSessionInUrl: true }
+  });
   return true;
 };
